1fc4ddfacc9c36ccc734338cf8e4864991e380c6
ESosa0 <ericasosa0@gmail.com>
2016-02-11 15:46:51 +0100

    added image to modal pop up

--- app/assets/javascripts/pokemon.js
+++ app/assets/javascripts/pokemon.js
@@ -9,26 +9,37 @@ $(function (){
   Pokemon.prototype.fetchPokemon = function(evnt) {
   
     var target = evnt.currentTarget; 
-    var url = $(target).attr("data-pokemon-uri")
-    $.get(url, this.renderPokemon);
+    var url = $(target).attr("data-pokemon-uri");
+    $.get(url, this.fetchImage.bind(this));
   };
 
-  Pokemon.prototype.renderPokemon = function (response) {
+  Pokemon.prototype.fetchImage = function (pokeinfo) {
+    var url = pokeinfo.sprites[0].resource_uri;
+    $.get(url, (function(spriteinfo){
+      this.renderPokemon(spriteinfo,pokeinfo);
+    }).bind(this));
+  };
+
+  Pokemon.prototype.renderPokemon = function (spriteinfo, pokeinfo) {
+
     $('.modal-title').empty()
     $('dd').empty()
     
-    var name = $(response).attr("name")
-    var height = $(response).attr("height")
-    var weight = $(response).attr("weight")
-    var attack = $(response).attr("attack")
+    var name = $(pokeinfo).attr("name")
+    var height = $(pokeinfo).attr("height")
+    var weight = $(pokeinfo).attr("weight")
+    var attack = $(pokeinfo).attr("attack")
+    var image = 'http://pokeapi.co' + spriteinfo.image
+
 
     $('.modal-title').prepend(name)
     $('.modal-title').append('<small> #' + attack + '</small')
     $('.height').append('<dd>' + height + '</dd>')
     $('.weight').append('<dd>' + weight + '</dd>')
+    $('.modal-title').append('<img src=' + '"' + image + '">')
     $('.modal').modal('show')
 
-  }
+  };
 
 
   var poke = new Pokemon();
